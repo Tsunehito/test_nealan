b7be28df842d2755a747435c037b3d2ca8d5d8fb
Tsunehito Higa <tsunehiga0310@gmail.com>
2019-01-23 22:42:25 +0100

    Creation of an associative arrays

--- test_nealan.php
+++ test_nealan.php
@@ -4,13 +4,14 @@ foreach (glob("../testTechnique/*.pcl") as $file) {
   $fileName[] = basename($file);
 }
 
-var_dump($fileName);
-
 for ($i = 0; $i < 20; $i++)
 {
   $path = "../testTechnique/" . $fileName[$i];
-  echo 'File name is ' . $fileName[$i] .
-        " size : " . filesize($path) ."\n";
+        $fileArray[$fileName[$i]] = filesize($path);
 }
 
- ?>
+// Tri le tableau associciatif par ordre décroissant 
+arsort($fileArray);
+foreach ($fileArray as $key => $value) {
+  echo "File name [$key] size [$value]\n";
+}
