f858007908f18d5bd5a19bd97e0e2f4a7a16b90c
Tsunehito Higa <tsunehiga0310@gmail.com>
2019-01-23 22:45:25 +0100

    added a comment

--- test_nealan.php
+++ test_nealan.php
@@ -4,13 +4,14 @@ foreach (glob("../testTechnique/*.pcl") as $file) {
   $fileName[] = basename($file);
 }
 
+// Crée un tableau associatif
 for ($i = 0; $i < 20; $i++)
 {
   $path = "../testTechnique/" . $fileName[$i];
         $fileArray[$fileName[$i]] = filesize($path);
 }
 
-// Tri le tableau associciatif par ordre décroissant 
+// Tri le tableau associciatif par ordre décroissant
 arsort($fileArray);
 foreach ($fileArray as $key => $value) {
   echo "File name [$key] size [$value]\n";
